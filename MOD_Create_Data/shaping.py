8b54a1bb3228d59ff206215bd0387a1a98e2ae17
Hydra <Hydra.echidna.009@gmail.com>
2023-01-28 14:17:52 +0900

    Shape program had changed
    Add the code for delete user tag and single-byte space

--- MOD_Create_Data/shaping.py
+++ MOD_Create_Data/shaping.py
@@ -5,13 +5,13 @@ import re
 data_list = []
 EXword_list = []
 
-with open('MOD_Create_Data\Exclusion-words.txt','r',encoding='UTF-8') as l:
+with open('./MOD_Create_Data/Exclusion-words.txt','r',encoding='UTF-8') as l:
     preEXword_list = l.readlines()
     for ex in preEXword_list:
         ex = ex.replace('\n', '')
         EXword_list.append(ex)
 
-with open('MOD_Create_Data\tweet\pre-conversation.json', 'r', encoding='UTF-8') as p:
+with open('./MOD_Create_Data/tweet/pre-conversation.json', 'r', encoding='UTF-8') as p:
     pre_learninngdata_list = json.load(p)
 
 data_index = len(pre_learninngdata_list) / 2
@@ -31,8 +31,8 @@ def txt_judge(REQ_txt, RES_txt):
 def txt_shape(REQ_txt, RES_txt):
     REQ_txt = REQ_txt.replace('\n','')
     RES_txt = RES_txt.replace('\n','')
-    re.sub('@[0-9a-zA-Z_]{1,15}', '', REQ_txt)
-    re.sub('@[0-9a-zA-Z_]{1,15}', '', RES_txt)
+    REQ_txt = re.sub('@[0-9a-zA-Z_]{1,15} ', '', REQ_txt)
+    RES_txt = re.sub('@[0-9a-zA-Z_]{1,15} ', '', RES_txt)
     return REQ_txt, RES_txt
 
 for i in range(int(data_index)):
@@ -43,5 +43,5 @@ for i in range(int(data_index)):
         continue
     
     REQ_txt, RES_txt = txt_shape(REQ_txt, RES_txt)
-    with open("MOD_Create_Data\tweet\learninng-data.txt", 'a', encoding='UTF-8') as fp:
+    with open("./MOD_Create_Data/tweet/learninng-data.txt", 'a', encoding='UTF-8') as fp:
         fp.write(f'REQ:{REQ_txt}\nRES:{RES_txt}\n')
